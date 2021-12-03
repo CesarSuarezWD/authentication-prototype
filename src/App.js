8a91304728f4be8fa02f106c550193dd4d2a9d21
CesarSuarezWD <cesarsurezmedina@gmail.com>
2021-12-03 14:34:00 -0500

    Primer Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
 import './App.css';
+import { useAuth0 } from '@auth0/auth0-react';
+import { LoginButton } from './Login';
+import { LogoutButton } from './Logout';
+import { Profile } from './Profile';
+import GETHIRED from './Icons/GETHIRED';
 
 function App() {
+
+const{ isAuthenticated } = useAuth0();
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {/* <img src="https://i.imgur.com/DlgU6sG.png"  alt="logo" /> */}
+        <GETHIRED />
+
+        
+
+        { isAuthenticated?( 
+        < >
+        
+        <Profile />
+        <LogoutButton />
+        
+        </>
+        ):(
+          
+          <LoginButton />
+          
+          )}     
+      
       </header>
+      <body className = "Body">
+        <div className = "contenedor-body">
+          <div className = "contenedor-hb">
+            {/* <h1 className = "h1-body">Bienvenido a GETHIRED</h1> */}
+            {/* <h3 className = "h3-body">Consigue el tarabajo de tus sueños!</h3> */}
+            {/* <button className = "contenedor-button">Empecemos</button> */}
+          </div>
+          {/* <img className = "img-body" src = "https://www.trabajin.com/blog/wp-content/uploads/2021/05/que-es-una-landing-page-670x419-1.png"></img> */}
+        </div>
+      </body>
+      <footer>
+      
+      </footer>
     </div>
   );
 }
